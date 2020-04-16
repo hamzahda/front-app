bc86cd98eadd0dd4f3afa91d1b6c55260ff247d8
Hamza <hamzahda@outlook.com>
2020-04-16 20:02:50 +0200

    adding views folder

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Form, Button, Container } from "react-bootstrap";
-import Result from "./Result";
+import Result from "../components/Result";
 
 function Login() {
   const [validated, setValidated] = useState(false);
